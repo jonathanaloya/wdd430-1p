2ac9633b9c5da27b5616e8478518280de069917c
jonathanaloya <alo23004@byui.edu>
2025-01-31 22:38:04 +0300

    try

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+
 import { updateInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
